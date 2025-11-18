c49de152db42b3d0c0eb2a885a492f88a72db91d
philkirkham101TestnQA <philkirkham101@gmail.com>
2025-11-18 15:23:31 -0500

    added new simple sauce test file

---
+++ tests/simple-sauce-tester.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+import { SauceDemoUsers } from '/utils/test-data';
+
+test('login with test data helper', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+
+  await page.locator('#user-name').fill(SauceDemoUsers.standard.username);
+  await page.locator('#password').fill(SauceDemoUsers.standard.password);
+  await page.locator('#login-button').click();
+
+  await expect(page).toHaveURL('/inventory.html');
+});
